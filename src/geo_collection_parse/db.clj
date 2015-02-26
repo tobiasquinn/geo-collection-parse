c2eb4bcf15cfda7765a3aa51185ea469e6737992
Tobias Quinn <tobias@tobiasquinn.com>
2015-02-26 01:29:09 +0000

    create spatial db and geometry column

--- src/geo_collection_parse/db.clj
+++ src/geo_collection_parse/db.clj
@@ -17,9 +17,24 @@
   (let [version (query con-db ["SELECT spatialite_version()"])]
     (println "spatialite driver version:" version)))
 
+(defn create-spatial-db! []
+  (println "Create spatial db")
+  (try (db-do-commands spatialite-db
+                       (create-table-ddl :locations
+                                         [:description :text]))
+       (catch Exception e (println e)))
+  (with-db-connection [con-db spatialite-db]
+    (query con-db ["SELECT load_extension('mod_spatialite')"])
+    (println "Creating spatial metadata")
+    (query con-db ["SELECT InitSpatialMetaData()"])
+    (println "Adding geometry column")
+    (let [result (query con-db ["SELECT AddGeometryColumn('locations', 'point', 4326, 'POINT', 'XY')"])]
+      (println "RESULTS:" result))))
+
 (defn- save-to-spatialite [record]
   (println "Should save:" record))
 
 ;; we want to read in the records and enter then into the spatial db
 (defn convert-geo-entries! []
-  (doall (map save-to-spatialite (query geo-collection-db ["SELECT * FROM locations"]))))
+  (create-spatial-db!))
+;  (doall (map save-to-spatialite (query geo-collection-db ["SELECT * FROM locations"]))))
